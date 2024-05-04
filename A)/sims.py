f74fe04dcb4e0e4ed49491b4aaff726780369719
Alejandro Álvarez Castro <116965920+AlexAlvarezCastro@users.noreply.github.com>
2024-05-04 20:54:25 +0200

    Update sims.py

--- A)/sims.py
+++ A)/sims.py
@@ -4,8 +4,7 @@ import tensorflow as tf
 import argparse
 #
 from Simulator import Simulator
-#
-from evidently.ui.workspace.cloud import CloudWorkspace
+from Data import Data
 
 
 tf.autograph.set_verbosity(0)
@@ -24,42 +23,13 @@ verbose_level = args.verbose
 
 #################################################################################################################################
 #################################################################################################################################
-                                            # CLOUD ENVIROMENT #
-#################################################################################################################################
-#################################################################################################################################
-
-ws = CloudWorkspace(
-token="dG9rbgF5sQ3u67hNVo7e7sL4fnJ4XmqN/OShAb/AhvcKVvPVdwBQ+rfox7G0ehgQfqSW3A+XWdfKYIoQMxrE7TcN0gBIU4Bia8KwfEtqBSx8hWwCrDno1orAqb5vNXp6e8Zr/KXRyydV8/ihwcIxDVePx4dc7XfrXF3i",
-url="https://app.evidently.cloud")
-project = ws.get_project("773856a9-b047-46c8-8c58-be21716e9369")
-
-#################################################################################################################################
-#################################################################################################################################
                                             # READ AND PREPARE DATA #
 #################################################################################################################################
 #################################################################################################################################
 
-train_data = np.load('/home/aacastro/Alejandro/DQ_ACA_2024/A)/ZN_1D_imgs/orig/train.npz') # CHANGE PATH
-train_f = train_data['features']
-train_l = train_data['labels']
-
-valid_data = np.load('/home/aacastro/Alejandro/DQ_ACA_2024/A)/ZN_1D_imgs/orig/validation.npz') # CHANGE PATH
-valid_f = valid_data['features']
-valid_l = valid_data['labels']
-
-test_data = np.load('/home/aacastro/Alejandro/DQ_ACA_2024/A)/ZN_1D_imgs/orig/test.npz') # CHANGE PATH
-test_f = test_data['features']
-test_l = test_data['labels']
-
-assess_data = np.load('/home/aacastro/Alejandro/DQ_ACA_2024/A)/ZN_1D_imgs/orig/assess.npz') # CHANGE PATH
-ass_f = assess_data['features']
-ass_l = assess_data['labels']
-
-x_train_valid = np.concatenate((train_f, valid_f), axis=0)
-y_train_valid = np.concatenate((train_l, valid_l), axis=0)
-
-X = np.concatenate((x_train_valid, test_f), axis=0)
-Y = np.concatenate((y_train_valid, test_l), axis=0)
+base_path = '/home/aacastro/Alejandro/DQ_ACA_2024/A)/ZN_1D_imgs/orig/'
+data_processor = Data(base_path)
+X, Y, ass_f, ass_l = data_processor.get_data('train.npz', 'validation.npz', 'test.npz', 'assess.npz')
 
 #################################################################################################################################
 #################################################################################################################################
@@ -73,8 +43,9 @@ NUM_FOLDERS = 2
 
 # Define the base path to store reults
 results_base_directory = "/home/aacastro/Alejandro/DQ_ACA_2024/A)/results"
+yaml_path = '/home/aacastro/Alejandro/DQ_ACA_2024/A)/configs.yaml'
 
-simulator = Simulator(ws, project, X, Y, ass_f, ass_l, verbose=verbose_level)
+simulator = Simulator(yaml_path, X, Y, ass_f, ass_l, verbose=verbose_level)
 
 model_metrics = simulator.simulate(NUM_SIMULATIONS, NUM_FOLDERS, results_base_directory)
 
@@ -88,4 +59,4 @@ for config_name, config_metrics in model_metrics.items():
             print(f"\tMean: {values['mean']}")
             print(f"\tStandard Deviation: {values['std_dev']}")
         else:
-            print(f"Execution Time: {model_metrics[config_name]['execution_time']} seconds")
+            print(f"Execution Time: {model_metrics[config_name]['execution_time']} seconds\n\n")
