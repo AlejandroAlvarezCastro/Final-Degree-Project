16dddc49c7f2f3816a888634947c05f6e2e27873
Alejandro Álvarez Castro <116965920+AlexAlvarezCastro@users.noreply.github.com>
2024-07-21 13:35:27 +0200

    Update Data.py

--- A)/Data.py
+++ A)/Data.py
@@ -1,16 +1,49 @@
 import numpy as np
 
-class Data():
+class Data:
     def __init__(self, base_path):
+        """
+        Initializes the Data object with a base path for loading data.
+
+        Parameters:
+            base_path (str): The base path where data files are located.
+        """
         self.base_path = base_path
 
     def load_data(self, filename):
+        """
+        Loads data from a .npz file and returns features and labels.
+
+        Parameters:
+            filename (str): The filename of the .npz file to load.
+
+        Returns:
+            tuple: A tuple containing:
+                - features (numpy.ndarray): The feature data.
+                - labels (numpy.ndarray): The label data.
+        """
         data = np.load(self.base_path + filename)
         features = data['features']
         labels = data['labels']
         return features, labels
 
     def get_data(self, train_filename, valid_filename, test_filename, assess_filename):
+        """
+        Loads and concatenates training, validation, and test data.
+
+        Parameters:
+            train_filename (str): The filename of the training data file.
+            valid_filename (str): The filename of the validation data file.
+            test_filename (str): The filename of the test data file.
+            assess_filename (str): The filename of the assessment data file.
+
+        Returns:
+            tuple: A tuple containing:
+                - X (numpy.ndarray): Concatenated features of training, validation, and test data.
+                - Y (numpy.ndarray): Concatenated labels of training, validation, and test data.
+                - ass_f (numpy.ndarray): Features of assessment data.
+                - ass_l (numpy.ndarray): Labels of assessment data.
+        """
         train_f, train_l = self.load_data(train_filename)
         valid_f, valid_l = self.load_data(valid_filename)
         test_f, test_l = self.load_data(test_filename)
@@ -23,6 +56,3 @@ class Data():
         Y = np.concatenate((y_train_valid, test_l), axis=0)
         
         return X, Y, ass_f, ass_l
-
-
-
